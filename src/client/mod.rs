9f33847ce16b1c4747d378b48f1342a958dea024
HeikoRibberink <heiko.ribberink@deblijdeaankomst.nl>
2022-10-14 09:05:34 +0200

    Plan changed: instead of making a single generic function for everything, make a function for every possible type and downcast it before usage.

--- src/client/mod.rs
+++ src/client/mod.rs
@@ -1,14 +1,34 @@
-use bevy::utils::Uuid;
+use bevy::prelude::*;
+use bevy::reflect::FromType;
+use bevy::{
+	utils::Uuid,
+};
 use hashbrown::HashMap;
 
-use crate::networking::{NetworkedEntity, Msg};
-use tokio_rustls::client::TlsStream;
+use crate::serialization::{ToServerMsg, ReflectToServerMsg};
+// use crate::serialization::components::{ReflectNetComponent, NetComponent};
+use crate::serialization::{bundle::NetBundle, NetEntity};
 use crossbeam_channel::Receiver;
+use postcard;
+use serde::{de::DeserializeSeed, Deserialize, Serialize, Serializer};
+use tokio_rustls::client::TlsStream;
 
 //Probably use the tokio runtime for IO (with tokio-rustls), send bevy events to that server, and receive events from the server.
 
 pub struct Client {
-	entity_registry: HashMap<Uuid, NetworkedEntity>,
-	//TODO: stream: TlsStream<Msg<>>,	
-	//TODO: msg_recv: Receiver<>,
+	entity_registry: HashMap<Uuid, NetEntity>,
+	//TODO: stream: TlsStream<Msg<>>,
+	// msg_recv: Receiver<Box<dyn ServerMsg>>,
 }
+
+impl Client {
+	pub fn sender_system(mut event_reader: EventReader<Box<dyn ToServerMsg>>, client: Res<Client>) {
+		for event in event_reader.iter() {
+			let event = (&**event);
+			let ser = postcard::to_stdvec(event);
+
+		}
+	}
+}
+
+//Maybe spawn entities with a custom [command](https://docs.rs/bevy/latest/bevy/prelude/struct.Commands.html#method.add)
